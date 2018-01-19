2a1abc25b018681251b1e38094288c76ea88c56a
Matthew J. Gorski <matt.gorski@gmail.com>
2018-01-19 13:29:07 -0500

    Create new atv branch with updated TVLauncher/LeanbackLauncher

--- atv/atv-common.mk
+++ atv/atv-common.mk
@@ -12,5 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_PACKAGES += LatinIMEGoogleTvPrebuilt \
-                    LeanbackLauncher
+PRODUCT_PACKAGES += AndroidPlatformServicesTV \
+		    LeanbackIme \
+                    LeanbackLauncher \
+                    SetupWraith \
+                    TVLauncher
